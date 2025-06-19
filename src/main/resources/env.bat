acf8cfe60143beb6a20e5e565c7d4434f0ee123d
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-06-19 11:17:37 -0300

    Update env.bat

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -5,10 +5,10 @@ setx SPRING_CLOUD_EUREKA_HOST "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT "8761" /M
 setx SPRING_CLOUD_EUREKA_PATH "/eureka" /M
 
-setx KEYCLOAK_REALM "fachesf-ad" /M
+setx KEYCLOAK_REALM "realm-empresa" /M
 setx KEYCLOAK_API_GATEWAY_CLIENT_ID "api-gateway" /M
 setx KEYCLOAK_API_GATEWAY_CLIENT_SECRET "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx KEYCLOAK_URI_SERVER "http://interno.empresa.com.br:11111/auth/realms/relm-empresa" /M
+setx KEYCLOAK_URI_SERVER "http://interno.empresa.com.br:11111/auth/realms/realm-empresa" /M
 
 echo Variaveis definidas com sucesso!
 pause
