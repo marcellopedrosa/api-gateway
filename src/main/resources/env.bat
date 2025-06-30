5a2973e58cc477674aa3544d4da686309a7db3db
Marcello Luiz Gomes do Egito Pedrosa <marcello.pedrosa@fachesf.com.br>
2025-06-30 12:02:12 -0300

    atualizacoes na configuracao no request client

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -2,18 +2,23 @@
 echo Definindo variáveis de ambiente...
 
 setx CONFIG_SERVER "http://localhost:8888" /M
-setx ZIPKIN_URL "http://lab.fachesf.com.br:9411/api/v2/spans" /M
-setx SERVER_LOG_HOST "logs.fachesf.com.br" /M
+setx CONFIG_SERVER_GIT_URI "http://localhost:8888" /M
+setx CONFIG_SERVER_GIT_SEARCH_PATHS "config" /M
+setx CONFIG_SERVER_GIT_USERNAME "admin" /M
+setx CONFIG_SERVER_GIT_PASSWORD "admin" /M
+
+setx ZIPKIN_URL "http://lab.empresa.com.br:9411/api/v2/spans" /M
+setx SERVER_LOG_HOST "logs.empresa.com.br" /M
 setx SERVER_LOG_PORT "12201" /M
 
 setx SPRING_CLOUD_EUREKA_HOST "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT "8761" /M
 setx SPRING_CLOUD_EUREKA_PATH "/eureka" /M
 
-setx KEYCLOAK_REALM "fachesf-ad" /M
-setx KEYCLOAK_API_GATEWAY_CLIENT_ID "api-gateway" /M
-setx KEYCLOAK_API_GATEWAY_CLIENT_SECRET "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx KEYCLOAK_URI_SERVER "http://localhost.com.br/auth/realms/fachesf-ad" /M
+setx SPI_REALM "empresa-ad" /M
+setx SPI_API_GATEWAY_CLIENT_ID "api-gateway" /M
+setx SPI_API_GATEWAY_CLIENT_SECRET "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
+setx SPI_URI_SERVER "http://localhost.com.br/auth/realms/empresa-ad" /M
 
 echo Variaveis definidas com sucesso!
 pause
