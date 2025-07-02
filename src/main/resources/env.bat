f0f7cef16b4d147a1851e6988f5b03e95f453d02
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-07-02 14:05:14 -0300

    Update env.bat

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -3,18 +3,18 @@ echo Definindo variáveis de ambiente...
 
 setx CONFIG_SERVER_URI "http://localhost:8888" /M
 
-setx ZIPKIN_URL "http://lab.fachesf.com.br:9411/api/v2/spans" /M
-setx SERVER_LOG_HOST "logs.fachesf.com.br" /M
+setx ZIPKIN_URL "http://lab.empresa.com.br:9411/api/v2/spans" /M
+setx SERVER_LOG_HOST "logs.empresa.com.br" /M
 setx SERVER_LOG_PORT "12201" /M
 
 setx SPRING_CLOUD_EUREKA_HOST "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT "8761" /M
 setx SPRING_CLOUD_EUREKA_PATH "/eureka" /M
 
-setx SPI_REALM "fachesf-ad" /M
+setx SPI_REALM "empresa-ad" /M
 setx SPI_API_GATEWAY_CLIENT_ID "api-gateway" /M
 setx SPI_API_GATEWAY_CLIENT_SECRET "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx SPI_URI_SERVER "http://localhost.com.br/auth/realms/fachesf-ad" /M
+setx SPI_URI_SERVER "http://localhost.com.br/auth/realms/empresa-ad" /M
 
 echo Variaveis definidas com sucesso!
 pause
