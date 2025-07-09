3c34bdf148a5eadb00c53f59dc55e1bfafe5c0c5
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-07-09 17:16:45 -0300

    Update env.bat

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -1,38 +1,56 @@
 @echo off
-echo Definindo variaveis de ambiente PRD/Producao...
+echo Definindo variaveis de ambiente DEV/local............
 
-setx GATEWAY_PORT "8080" /M
-setx CONFIG_SERVER_URI "http://localhost:8888" /M
-setx WEB_SERVER "http://localhost.com.br" /M
-setx ZIPKIN_URL "http://lab.empresa.com.br:9411/api/v2/spans" /M
+setx GATEWAY_PORT_DEV "8080" /M
+setx CONFIG_SERVER_URI_DEV "http://localhost:8888" /M
+setx WEB_SERVER_DEV "http://localhost.com.br" /M
+setx ZIPKIN_URL_DEV "http://lab.empresa.com.br:9411/api/v2/spans" /M
 
-echo Definindo Server Log PRD/QA eh centralizado
+setx SPI_REALM_DEV "empresa-ad" /M
+setx SPI_API_GATEWAY_CLIENT_ID_DEV "api-gateway" /M
+setx SPI_API_GATEWAY_CLIENT_SECRET_DEV "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
+setx SPI_URI_SERVER_DEV "http://localhost.com.br/auth/realms/empresa-ad" /M
+
+setx SPRING_CLOUD_EUREKA_HOST_DEV "http://localhost" /M
+setx SPRING_CLOUD_EUREKA_PORT_DEV "8761" /M
+setx SPRING_CLOUD_EUREKA_PATH_DEV "/eureka" /M
+
+echo Definindo variaveis de ambiente HML/Homologacao............
+
+setx GATEWAY_PORT_HML "8080" /M
+setx CONFIG_SERVER_URI_HML "http://localhost:8888" /M
+setx WEB_SERVER_HML "http://localhost.com.br" /M
+setx ZIPKIN_URL_HML "http://lab.empresa.com.br:9411/api/v2/spans" /M
+
+setx SPI_REALM_HML "empresa-ad" /M
+setx SPI_API_GATEWAY_CLIENT_ID_HML "api-gateway" /M
+setx SPI_API_GATEWAY_CLIENT_SECRET_HML "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
+setx SPI_URI_SERVER_HML "http://localhost.com.br/auth/realms/empresa-ad" /M
+
+setx SPRING_CLOUD_EUREKA_HOST_HML "http://localhost" /M
+setx SPRING_CLOUD_EUREKA_PORT_HML "8761" /M
+setx SPRING_CLOUD_EUREKA_PATH_HML "/eureka" /M
+
+echo Definindo variaveis de ambiente PRD/Producao............
+
+setx GATEWAY_PORT_PRD "8080" /M
+setx CONFIG_SERVER_URI_PRD "http://localhost:8888" /M
+setx WEB_SERVER_PRD "http://localhost.com.br" /M
+setx ZIPKIN_URL_PRD "http://lab.empresa.com.br:9411/api/v2/spans" /M
+
+setx SPI_REALM_PRD "empresa-ad" /M
+setx SPI_API_GATEWAY_CLIENT_ID_PRD "api-gateway" /M
+setx SPI_API_GATEWAY_CLIENT_SECRET_PRD "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
+setx SPI_URI_SERVER_PRD "http://localhost.com.br/auth/realms/empresa-ad" /M
+
+setx SPRING_CLOUD_EUREKA_HOST_PRD "http://localhost" /M
+setx SPRING_CLOUD_EUREKA_PORT_PRD "8761" /M
+setx SPRING_CLOUD_EUREKA_PATH_PRD "/eureka" /M
+
+echo Definindo variaveis SERVER LOG............
+
 setx SERVER_LOG_HOST "logs.empresa.com.br" /M
 setx SERVER_LOG_PORT "12201" /M
-
-setx SPI_REALM "empresa-ad" /M
-setx SPI_API_GATEWAY_CLIENT_ID "api-gateway" /M
-setx SPI_API_GATEWAY_CLIENT_SECRET "01n2ug8OELqcP77NH0KoLzSCMc06OkwG" /M
-setx SPI_URI_SERVER "http://localhost.com.br/auth/realms/empresa-ad" /M
-
-setx SPRING_CLOUD_EUREKA_HOST "http://localhost" /M
-setx SPRING_CLOUD_EUREKA_PORT "8761" /M
-setx SPRING_CLOUD_EUREKA_PATH "/eureka" /M
-
-echo Definindo variaveis de ambiente QA/Qualidade/Homologacao...
-
-setx GATEWAY_PORT_QA "8080" /M
-setx CONFIG_SERVER_URI_QA "http://localhost:8888" /M
-setx WEB_SERVER_QA "http://localhost.com.br" /M
-setx ZIPKIN_URL_QA "http://lab.empresa.com.br:9411/api/v2/spans" /M
-
-setx SPI_REALM_QA "empresa-ad" /M
-setx SPI_URI_SERVER_QA "http://localhost.com.br/auth/realms/empresa-ad"
-setx SPI_API_GATEWAY_CLIENT_SECRET_QA "01n2ug8OELqcP77NH0KoLzSCMc06OkwG" /M
-
-setx SPRING_CLOUD_EUREKA_HOST_QA "http://localhost" /M
-setx SPRING_CLOUD_EUREKA_PORT_QA "8761" /M
-setx SPRING_CLOUD_EUREKA_PATH_QA "/eureka" /M
 
 echo Variaveis definidas com sucesso!
 pause
