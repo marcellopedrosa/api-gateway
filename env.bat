3cc2119c0846ea32ad16bef36f758e6e99f0e97e
Marcello Luiz Gomes do Egito Pedrosa <marcello.pedrosa@fachesf.com.br>
2025-07-14 09:06:01 -0300

    ajustes na variaveis locais e local do .env e .yml

--- env.bat
+++ env.bat
@@ -1,15 +1,14 @@
 @echo off
 echo Definindo variaveis de ambiente DEV/local............
 
-setx GATEWAY_PORT_DEV "8080" /M
 setx CONFIG_SERVER_URI_DEV "http://localhost:8888" /M
-setx WEB_SERVER_DEV "http://localhost.com.br" /M
+setx WEB_SERVER_DEV "http://localhost.dev.com.br" /M
 setx ZIPKIN_URL_DEV "http://lab.empresa.com.br:9411/api/v2/spans" /M
 
 setx SPI_REALM_DEV "empresa-ad" /M
 setx SPI_API_GATEWAY_CLIENT_ID_DEV "api-gateway" /M
 setx SPI_API_GATEWAY_CLIENT_SECRET_DEV "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx SPI_URI_SERVER_DEV "http://localhost.com.br/auth/realms/empresa-ad" /M
+setx SPI_URI_SERVER_DEV "http://localhost.dev.com.br/auth/realms/empresa-ad" /M
 
 setx SPRING_CLOUD_EUREKA_HOST_DEV "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT_DEV "8761" /M
@@ -17,15 +16,14 @@ setx SPRING_CLOUD_EUREKA_PATH_DEV "/eureka" /M
 
 echo Definindo variaveis de ambiente HML/Homologacao............
 
-setx GATEWAY_PORT_HML "8080" /M
 setx CONFIG_SERVER_URI_HML "http://localhost:8888" /M
-setx WEB_SERVER_HML "http://localhost.com.br" /M
+setx WEB_SERVER_HML "http://localhost.dev.com.br" /M
 setx ZIPKIN_URL_HML "http://lab.empresa.com.br:9411/api/v2/spans" /M
 
 setx SPI_REALM_HML "empresa-ad" /M
 setx SPI_API_GATEWAY_CLIENT_ID_HML "api-gateway" /M
 setx SPI_API_GATEWAY_CLIENT_SECRET_HML "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx SPI_URI_SERVER_HML "http://localhost.com.br/auth/realms/empresa-ad" /M
+setx SPI_URI_SERVER_HML "http://localhost.dev.com.br/auth/realms/empresa-ad" /M
 
 setx SPRING_CLOUD_EUREKA_HOST_HML "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT_HML "8761" /M
@@ -33,15 +31,14 @@ setx SPRING_CLOUD_EUREKA_PATH_HML "/eureka" /M
 
 echo Definindo variaveis de ambiente PRD/Producao............
 
-setx GATEWAY_PORT_PRD "8080" /M
 setx CONFIG_SERVER_URI_PRD "http://localhost:8888" /M
-setx WEB_SERVER_PRD "http://localhost.com.br" /M
+setx WEB_SERVER_PRD "http://localhost.dev.com.br" /M
 setx ZIPKIN_URL_PRD "http://lab.empresa.com.br:9411/api/v2/spans" /M
 
 setx SPI_REALM_PRD "empresa-ad" /M
 setx SPI_API_GATEWAY_CLIENT_ID_PRD "api-gateway" /M
 setx SPI_API_GATEWAY_CLIENT_SECRET_PRD "YuqVdLA9Kr9LpLKcsU4Cyo9HzvHEW9f5" /M
-setx SPI_URI_SERVER_PRD "http://localhost.com.br/auth/realms/empresa-ad" /M
+setx SPI_URI_SERVER_PRD "http://localhost.dev.com.br/auth/realms/empresa-ad" /M
 
 setx SPRING_CLOUD_EUREKA_HOST_PRD "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT_PRD "8761" /M
